e4a08119acf442ab82a411e7ddc7648ab2cda59d
Yuantai Du <duyt1001@gmail.com>
2023-12-16 17:37:35 -0500

    Update skip-youtube-ads.user.js
    fine tune

--- monkey/skip-youtube-ads.user.js
+++ monkey/skip-youtube-ads.user.js
@@ -8,12 +8,12 @@
 // ==/UserScript==
 // from https://www.reddit.com/r/qutebrowser/comments/ntl2ko/easy_youtube_adblocker_greasemonkey_script/
 setInterval(() => {
-    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
+    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
     if (btn) {
         btn.click()
     }
     const ad = [...document.querySelectorAll('.ad-showing')][0];
     if (ad) {
-        document.querySelector('video').playbackRate = 10;
+        document.querySelector('video').playbackRate = 16;
     }
 }, 50)
